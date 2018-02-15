e9be545f6511ad23e8ba91192862fe222bd3e640
Javier R <frjaraur@gmail.com>
2018-02-15 21:20:32 +0100

    Fixed nginx port

--- deploy_storage_ring.sh
+++ deploy_storage_ring.sh
@@ -34,7 +34,7 @@ minio/minio server http://minio-1/data http://minio-2/data http://minio-3/data h
 ## Deploying using host entries because we don't use DNS
 echo "Deploy Minio-NGINX Service (accesible on port 9001"
 docker service create --name minio-nginx \
---publish 9001:9000 \
+--publish 9001:9001 \
 --host "minio-1:10.10.10.11" \
 --host "minio-2:10.10.10.12" \
 --host "minio-3:10.10.10.13" \
